25dba4fc4d71363a94bd805748a8790fcdc723dd
Alex <chugun777akk@mail.ru>
2022-03-22 19:53:04 +0700

    task1 is done

---
+++ HW4task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+void array_on_Display(int[] array)
+{
+    Console.Write("Сгенерированный массив:  " + '[');
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(']');
+}
+void Even_Odd_count(int[] array)
+{
+    int count_Even = 0;
+    int count_Odd = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count_Even++;
+        }
+        else
+        {
+            count_Odd++;
+        }
+    }
+    Console.WriteLine("\nКоличество четных чисел в массиве: " + count_Even);
+    Console.WriteLine("Количество нечетных чисел в массиве: " + count_Odd);
+}
+
+/////////////////////////////////////////////////////////////////////////
+int[] array = new int[10];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+}
+array_on_Display(array);
+Even_Odd_count(array);
+return 0;
