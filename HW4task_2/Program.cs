8bf590a86a1e218336dda6bce72225b2dc601dd4
Alex <chugun777akk@mail.ru>
2022-03-22 20:33:14 +0700

    task2 is done

---
+++ HW4task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+void array_on_Display(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+int count_element(int[] arr)
+{
+    int count_element = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] <= 99)
+        {
+            count_element++;
+        }
+    }
+    return count_element;
+}
+
+int[] array = new int[123];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 1000);
+}
+array_on_Display(array);
+int Total_count = count_element(array);
+Console.WriteLine("\nКоличество елеметов на отрезке [10,99] равно:  " + Total_count);
